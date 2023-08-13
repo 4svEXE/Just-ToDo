8edb9f2397e2ebff9e3f802a2a16aa10cad2a1f1
Иван Сташко <4sv.exe@gmail.com>
2023-08-13 17:14:34 +0300

    set new proj

---
+++ src/app/services/tasks.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+
+import { TaskInterface } from "./../interfaces/index";
+
+@Injectable({
+  providedIn: "root",
+})
+export class TasksService {
+  private readonly LSTableName = "tasks";
+  tasks!: TaskInterface[];
+
+  ///////////////////////////////////////
+  private tasksData: any[] = [];
+
+  private tasksDataObservable: Observable<any[]> = new Observable(
+    (observer) => {
+      observer.next(this.tasksData);
+    }
+  );
+  private dataSubject: BehaviorSubject<any[]> = new BehaviorSubject(
+    this.tasksData
+  );
+
+  getDataObservable(): Observable<any[]> {
+    return this.tasksDataObservable;
+  }
+
+  addData(newData: any): void {
+    this.tasksData = newData;
+    this.dataSubject.next(this.tasksData);
+  }
+  ///////////////////////////////////////
+
+  constructor() {}
+
+  getTasks() {
+    let Tasks = localStorage.getItem(this.LSTableName) || "[]";
+    return JSON.parse(Tasks);
+  }
+
+  setTasks(tasks: TaskInterface[]) {
+    const Tasks = JSON.stringify(tasks);
+    localStorage.setItem(this.LSTableName, Tasks);
+  }
+
+  setTask(task: TaskInterface) {
+    let Tasks = this.getTasks();
+    Tasks.push(task);
+    this.setTasks(Tasks);
+  }
+
+  deleteTask(id: string) {
+    let Tasks = this.getTasks();
+
+    Tasks = Tasks.filter((task: TaskInterface) => {
+      return !(task.id === id);
+    });
+
+    this.setTasks(Tasks);
+  }
+
+  toggleCheckbox(id: string) {
+    let Tasks = this.getTasks();
+
+    Tasks = Tasks.map((task: TaskInterface) => {
+      if (task.id === id) {
+        task.isChecked = !task.isChecked;
+      }
+    });
+
+    this.setTasks(Tasks);
+  }
+}
